389fdab1e6561fdb4ff201e21a42c3949b61ff95
NokOut-Games <nokout.acc@gmail.com>
2021-09-24 13:59:05 +0530

    Today's thing

--- Assets/Scripts/CardDeck.cs
+++ Assets/Scripts/CardDeck.cs
@@ -10,9 +10,11 @@ public class CardDeck : MonoBehaviour
 
     [SerializeField] LevelManagerUI levelManagerUI;
 
-    public List<Transform> _cardSlotPoints;
+    //public List<Transform> _cardSlotPoints;
     int clicks = 0;
     [SerializeField] Cards mCards;
+
+    public List<HandPoints> handPoints;
 
     private void Start()
     {
@@ -25,32 +27,69 @@ public class CardDeck : MonoBehaviour
     public void DrawCard()
     {
         ZoomCameraToPlayArea();
-        GameObject card = Instantiate(mCards.boosterCards[Random.Range(0, mCards.boosterCards.Count)]._cardModel, _cardSlotPoints[clicks].position, Quaternion.Euler(180f, -90f, 0f));
-        switch (card.tag)
+        if(!handPoints[clicks].isFilled)
         {
-            case "5K Coins":
-                levelManagerUI.FiveK.Add(card);
-                break;
-            case "25K Coins":
-                levelManagerUI.TWENTYFIVEK.Add(card);
-                break;
-            case "100K Coins":
-                levelManagerUI.HUNDREDK.Add(card);
-                break;
-            case "500K Coins":
-                levelManagerUI.FIVEHUNDREDK.Add(card);
-                break;
-            case "1M Coins":
-                levelManagerUI.ONEM.Add(card);
-                break;
+            GameObject card = Instantiate(mCards.boosterCards[Random.Range(0, mCards.boosterCards.Count)]._cardModel, handPoints[clicks].transform.position, Quaternion.Euler(180f, -90f, 0f));
+            handPoints[clicks].isFilled = true;
+            switch (card.tag)
+            {
+                case "5K Coins":
+                    levelManagerUI.FiveK.Add(card);
+                    //levelManagerUI.OverAllCards.Add(card);
+                    break;
+                case "25K Coins":
+                    levelManagerUI.TWENTYFIVEK.Add(card);
+                    //levelManagerUI.OverAllCards.Add(card);
+                    break;
+                case "100K Coins":
+                    levelManagerUI.HUNDREDK.Add(card);
+                    //levelManagerUI.OverAllCards.Add(card);
+                    break;
+                case "500K Coins":
+                    levelManagerUI.FIVEHUNDREDK.Add(card);
+                    //levelManagerUI.OverAllCards.Add(card);
+                    break;
+                case "1M Coins":
+                    levelManagerUI.ONEM.Add(card);
+                    //levelManagerUI.OverAllCards.Add(card);
+                    break;
+            }
+            clicks += 1;
         }
-        clicks += 1;
     }
 
     private void Update()
     {
-        if (clicks > 7)
+        if (clicks == 8)
         {
+            //levelManagerUI.OverAllCards.Clear();
+            foreach (GameObject card in levelManagerUI.FiveK)
+            {
+                levelManagerUI.FiveK.Clear();
+                Destroy(card);
+            }
+            foreach (GameObject card in levelManagerUI.TWENTYFIVEK)
+            {
+                levelManagerUI.TWENTYFIVEK.Clear();
+                Destroy(card);
+            }
+            foreach (GameObject card in levelManagerUI.HUNDREDK)
+            {
+                levelManagerUI.HUNDREDK.Clear();
+                Destroy(card);
+            }
+            foreach (GameObject card in levelManagerUI.FIVEHUNDREDK)
+            {
+                levelManagerUI.FIVEHUNDREDK.Clear();
+                Destroy(card);
+            }
+            foreach (GameObject card in levelManagerUI.ONEM)
+            {
+                levelManagerUI.ONEM.Clear();
+                Destroy(card);
+            }
+
+            clicks = 0;
             return;
         }
     }
--- Assets/Scripts/LevelManagerUI.cs
+++ Assets/Scripts/LevelManagerUI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LevelManagerUI : MonoBehaviour
 {
+    public List<GameObject> OverAllCards;
+
     public List<GameObject> FiveK;
     public List<GameObject> TWENTYFIVEK;
     public List<GameObject> HUNDREDK;
@@ -12,6 +14,8 @@ public class LevelManagerUI : MonoBehaviour
 
     public GameManager gameManager;
 
+    public bool isDone = false; 
+
     private void Start()
     {
  
@@ -19,9 +23,88 @@ public class LevelManagerUI : MonoBehaviour
 
     private void Update()
     {
-        if (FiveK.Count == 3)
+        if (!isDone)
         {
-            gameManager._coins += 5000;
+            if (FiveK.Count == 3)
+            {
+                gameManager._coins += 5000;
+                isDone = true;
+                foreach (GameObject c in FiveK)
+                {
+                    //PlayAnimation
+                    Destroy(c);
+                }
+                FiveK.Clear();
+            }
+            if (TWENTYFIVEK.Count == 3)
+            {
+                gameManager._coins += 25000;
+                isDone = true;
+                foreach (GameObject c in TWENTYFIVEK)
+                {
+                    //PlayAnimation
+                    Destroy(c);
+                }
+                TWENTYFIVEK.Clear();
+            }
+            if (HUNDREDK.Count == 3)
+            {
+                gameManager._coins += 100000;
+                isDone = true;
+                foreach (GameObject c in HUNDREDK)
+                {
+                    //PlayAnimation
+                    Destroy(c);
+                }
+                HUNDREDK.Clear();
+            }
+            if (FIVEHUNDREDK.Count == 3)
+            {
+                gameManager._coins += 500000;
+                isDone = true;
+                foreach (GameObject c in FIVEHUNDREDK)
+                {
+                    //PlayAnimation
+                    Destroy(c);
+                }
+                FIVEHUNDREDK.Clear();
+            }
+            if (ONEM.Count == 3)
+            {
+                gameManager._coins += 1000000;
+                isDone = true;
+                foreach (GameObject c in ONEM)
+                {
+                    //PlayAnimation
+                    Destroy(c);
+                }
+                ONEM.Clear();
+            }
         }
+        //if (FiveK.Count == 3)
+        //{
+        //    gameManager._coins += 5000;
+        //    FiveK.Clear();
+        //}
+        //if (TWENTYFIVEK.Count == 3)
+        //{
+        //    gameManager._coins += 25000;
+        //    TWENTYFIVEK.Clear();
+        //}
+        //if (HUNDREDK.Count == 3)
+        //{
+        //    gameManager._coins += 100000;
+        //    HUNDREDK.Clear();
+        //}
+        //if (FIVEHUNDREDK.Count == 3)
+        //{
+        //    gameManager._coins += 500000;
+        //    FIVEHUNDREDK.Clear();
+        //}
+        //if (ONEM.Count == 3)
+        //{
+        //    gameManager._coins += 1000000;
+        //    ONEM.Clear();
+        //}
     }
 }
