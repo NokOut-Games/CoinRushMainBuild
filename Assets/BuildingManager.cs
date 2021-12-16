47cc31ac10d4f1ec4c191e40e12d812d48d82c7e
IcyTStark <pavansai432@gmail.com>
2021-12-16 21:09:18 +0530

    Fixed Finding the building

--- Assets/BuildingManager.cs
+++ Assets/BuildingManager.cs
@@ -28,10 +28,6 @@ public class BuildingManager : MonoBehaviour
 {
     public BuildingData[] _buildingData;
     public List<GameObject> _buildingsList;
-
-    int i = 0;
-    int j = 0;
-    int k = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -40,7 +36,7 @@ public class BuildingManager : MonoBehaviour
         {
             GameObject GORef = Instantiate(_buildingData[i]._initialBuildingGameObject, _buildingData[i]._buildingSpawnPoint.position, _buildingData[i]._buildingSpawnPoint.rotation);
             GORef.name = _buildingData[i]._buildingName;
-            
+
             _buildingsList.Add(GORef);
         }
     }
@@ -57,6 +53,11 @@ public class BuildingManager : MonoBehaviour
         GameObject goRef = GameObject.Find(name);
         Debug.Log(goRef);
         Destroy(goRef);
+        //if (inLevel != 0)
+        //{
+        //    GameObject oldMesh = inCurrentLevelsMesh;
+        //    Destroy(oldMesh);   
+        //}
         GameObject newGoRef = Instantiate(_buildingData[inBuildingNumber].UpgradeLevels[inLevel], _buildingData[inBuildingNumber]._buildingSpawnPoint.position, _buildingData[inBuildingNumber]._buildingSpawnPoint.rotation);
         newGoRef.name = _buildingData[inBuildingNumber]._buildingName;
         newGoRef.AddComponent<BuildingDetails>();
@@ -71,103 +72,103 @@ public class BuildingManager : MonoBehaviour
         {
             UpgradeBuilding(_buildingData[0]._buildingName, 0, _buildingData[0]._buildingLevel,_buildingData[0].UpgradeLevels[_buildingData[0]._buildingLevel]);
             _buildingData[0]._buildingLevel += 1;
-        }
+        }                                   
         else
         {
             _buildingData[0].didBuildingReachMaxLevel = true;
         }
     }
 
-    //public void Building2Upgrade()
-    //{
-    //    if (_buildingData[1]._buildingLevel < _buildingData[1].UpgradeLevels.Length && _buildingData[1]._buildingLevel < _buildingData[1]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[1]._buildingName, 1, _buildingData[1]._buildingLevel, _buildingData[1].UpgradeLevels[_buildingData[1]._buildingLevel]);
-    //        _buildingData[1]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[1].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building2Upgrade()
+    {
+        if (_buildingData[1]._buildingLevel < _buildingData[1].UpgradeLevels.Length && _buildingData[1]._buildingLevel < _buildingData[1]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[1]._buildingName, 1, _buildingData[1]._buildingLevel, _buildingData[1].UpgradeLevels[_buildingData[1]._buildingLevel]);
+            _buildingData[1]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[1].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building3Upgrade()
-    //{
-    //    if (_buildingData[2]._buildingLevel < _buildingData[2].UpgradeLevels.Length && _buildingData[2]._buildingLevel < _buildingData[2]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[2]._buildingName, 2, _buildingData[2]._buildingLevel, _buildingData[2].UpgradeLevels[_buildingData[2]._buildingLevel]);
-    //        _buildingData[2]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[2].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building3Upgrade()
+    {
+        if (_buildingData[2]._buildingLevel < _buildingData[2].UpgradeLevels.Length && _buildingData[2]._buildingLevel < _buildingData[2]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[2]._buildingName, 2, _buildingData[2]._buildingLevel, _buildingData[2].UpgradeLevels[_buildingData[2]._buildingLevel]);
+            _buildingData[2]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[2].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building4Upgrade()
-    //{
-    //    if (_buildingData[3]._buildingLevel < _buildingData[3].UpgradeLevels.Length && _buildingData[3]._buildingLevel < _buildingData[3]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[3]._buildingName, 3, _buildingData[3]._buildingLevel, _buildingData[3].UpgradeLevels[_buildingData[3]._buildingLevel]);
-    //        _buildingData[3]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[3].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building4Upgrade()
+    {
+        if (_buildingData[3]._buildingLevel < _buildingData[3].UpgradeLevels.Length && _buildingData[3]._buildingLevel < _buildingData[3]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[3]._buildingName, 3, _buildingData[3]._buildingLevel, _buildingData[3].UpgradeLevels[_buildingData[3]._buildingLevel]);
+            _buildingData[3]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[3].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building5Upgrade()
-    //{
-    //    if (_buildingData[4]._buildingLevel < _buildingData[4].UpgradeLevels.Length && _buildingData[4]._buildingLevel < _buildingData[4]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[4]._buildingName, 4, _buildingData[4]._buildingLevel, _buildingData[4].UpgradeLevels[_buildingData[4]._buildingLevel]);
-    //        _buildingData[4]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[4].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building5Upgrade()
+    {
+        if (_buildingData[4]._buildingLevel < _buildingData[4].UpgradeLevels.Length && _buildingData[4]._buildingLevel < _buildingData[4]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[4]._buildingName, 4, _buildingData[4]._buildingLevel, _buildingData[4].UpgradeLevels[_buildingData[4]._buildingLevel]);
+            _buildingData[4]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[4].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building6Upgrade()
-    //{
-    //    if (_buildingData[5]._buildingLevel < _buildingData[5].UpgradeLevels.Length && _buildingData[5]._buildingLevel < _buildingData[5]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[5]._buildingName, 5, _buildingData[5]._buildingLevel, _buildingData[5].UpgradeLevels[_buildingData[5]._buildingLevel]);
-    //        _buildingData[5]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[5].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building6Upgrade()
+    {
+        if (_buildingData[5]._buildingLevel < _buildingData[5].UpgradeLevels.Length && _buildingData[5]._buildingLevel < _buildingData[5]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[5]._buildingName, 5, _buildingData[5]._buildingLevel, _buildingData[5].UpgradeLevels[_buildingData[5]._buildingLevel]);
+            _buildingData[5]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[5].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building7Upgrade()
-    //{
-    //    if (_buildingData[6]._buildingLevel < _buildingData[6].UpgradeLevels.Length && _buildingData[6]._buildingLevel < _buildingData[6]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[6]._buildingName, 6, _buildingData[6]._buildingLevel, _buildingData[6].UpgradeLevels[_buildingData[6]._buildingLevel]);
-    //        _buildingData[6]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[6].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building7Upgrade()
+    {
+        if (_buildingData[6]._buildingLevel < _buildingData[6].UpgradeLevels.Length && _buildingData[6]._buildingLevel < _buildingData[6]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[6]._buildingName, 6, _buildingData[6]._buildingLevel, _buildingData[6].UpgradeLevels[_buildingData[6]._buildingLevel]);
+            _buildingData[6]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[6].didBuildingReachMaxLevel = true;
+        }
+    }
 
-    //public void Building8Upgrade()
-    //{
-    //    if (_buildingData[7]._buildingLevel < _buildingData[7].UpgradeLevels.Length && _buildingData[7]._buildingLevel < _buildingData[7]._buildingMaxLevel)
-    //    {
-    //        UpgradeBuilding(_buildingData[7]._buildingName, 7, _buildingData[7]._buildingLevel, _buildingData[7].UpgradeLevels[_buildingData[7]._buildingLevel]);
-    //        _buildingData[7]._buildingLevel += 1;
-    //    }
-    //    else
-    //    {
-    //        _buildingData[7].didBuildingReachMaxLevel = true;
-    //    }
-    //}
+    public void Building8Upgrade()
+    {
+        if (_buildingData[7]._buildingLevel < _buildingData[7].UpgradeLevels.Length && _buildingData[7]._buildingLevel < _buildingData[7]._buildingMaxLevel)
+        {
+            UpgradeBuilding(_buildingData[7]._buildingName, 7, _buildingData[7]._buildingLevel, _buildingData[7].UpgradeLevels[_buildingData[7]._buildingLevel]);
+            _buildingData[7]._buildingLevel += 1;
+        }
+        else
+        {
+            _buildingData[7].didBuildingReachMaxLevel = true;
+        }
+    }
 }
 
 
